57b7638cd1bb90cb6f352f45e5938cff9d184033
pyronasaur <pyronnicholas@gmail.com>
2019-10-09 23:28:41 -0400

    Updates

---
+++ precache-manifest.7bc27b37ca95cf813f53ffa4a94f36bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "099333d587a1cf6a491ddfeb2a56c64c",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "463d9814a846ef5681d6",
+    "url": "/clickygame/static/css/main.2e132b0f.chunk.css"
+  },
+  {
+    "revision": "7bcda612e7e3167e85cf",
+    "url": "/clickygame/static/js/2.fc710326.chunk.js"
+  },
+  {
+    "revision": "463d9814a846ef5681d6",
+    "url": "/clickygame/static/js/main.075d8953.chunk.js"
+  },
+  {
+    "revision": "67274fd135527b551f64",
+    "url": "/clickygame/static/js/runtime-main.1224341a.js"
+  },
+  {
+    "revision": "203aa84f70a1b725557ef3f5d3e3dc29",
+    "url": "/clickygame/static/media/leaves.203aa84f.png"
+  }
+]);
